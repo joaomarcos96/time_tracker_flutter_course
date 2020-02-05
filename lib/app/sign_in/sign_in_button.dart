60d6ec50f11758672b093d5247a5e994114e41da
joaomarcos96 <jao.markos@hotmail.com>
2020-02-04 22:06:23 -0300

    Add images and sign in buttons

---
+++ lib/app/sign_in/sign_in_button.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/common_widgets/custom_raised_button.dart';
+
+class SignInButton extends CustomRaisedButton {
+  SignInButton({
+    String text,
+    Color color,
+    Color textColor,
+    VoidCallback onPressed,
+  }) : super(
+          child: Text(
+            text,
+            style: TextStyle(
+              color: textColor,
+              fontSize: 16,
+            ),
+          ),
+          color: color,
+          onPressed: onPressed,
+        );
+}
--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/sign_in_button.dart';
+import 'package:time_tracker_flutter_course/common_widgets/custom_raised_button.dart';
 
 class SignInPage extends StatelessWidget {
   @override
@@ -8,33 +10,84 @@ class SignInPage extends StatelessWidget {
         title: Text('Time Tracker'),
         elevation: 2,
       ),
-      body: buildContent(),
+      body: _buildContent(),
+      backgroundColor: Colors.grey[200],
     );
   }
 
-  Widget buildContent() {
-    return Container(
-      color: Colors.yellow,
+  Widget _buildContent() {
+    return Padding(
+      padding: EdgeInsets.all(16),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Container(
-            color: Colors.orange,
-            child: SizedBox(
-              height: 100,
+          Text(
+            'Sign in',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 32, fontWeight: FontWeight.w600),
+          ),
+          SizedBox(
+            height: 48,
+          ),
+          CustomRaisedButton(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Image.asset('images/google-logo.png'),
+                Text('Sign in with Google'),
+                SizedBox(),
+              ],
+            ),
+            color: Colors.white,
+            onPressed: () {},
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          SignInButton(
+            text: 'Sign in with Google',
+            textColor: Colors.black87,
+            color: Colors.white,
+            onPressed: () {},
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          SignInButton(
+            text: 'Sign in with Facebook',
+            textColor: Colors.white,
+            color: Color(0xFF334D92),
+            onPressed: () {},
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          SignInButton(
+            text: 'Sign in with email',
+            textColor: Colors.white,
+            color: Colors.teal[700],
+            onPressed: () {},
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Text(
+            'or',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 14,
+              color: Colors.black87,
             ),
           ),
-          Container(
-            color: Colors.red,
-            child: SizedBox(
-              height: 100,
-            ),
+          SizedBox(
+            height: 8,
           ),
-          Container(
-            color: Colors.purple,
-            child: SizedBox(
-              height: 100,
-            ),
+          SignInButton(
+            text: 'Go anonymous',
+            textColor: Colors.black,
+            color: Colors.lime[300],
+            onPressed: () {},
           ),
         ],
       ),
